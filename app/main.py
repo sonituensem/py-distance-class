cbba0c8c643721da19603cf1b8bb4cd7ea4ba0c1
Yatsenko Dmytro <sonituensem@gmail.com>
2025-11-17 11:56:41 +0200

    Fixed flake8 issues

--- app/main.py
+++ app/main.py
@@ -1,45 +1,45 @@
 class Distance:
-    def __init__(self, km):
-        self.km = km
+    def __init__(self, km: float) -> None:
+        self.km: float = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: float) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: float) -> "Distance":
         return Distance(round(self.km / other, 2))
 
-    def _value(self, other):
+    def _value(self, other) -> float:
         return other.km if isinstance(other, Distance) else other
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         return self.km < self._value(other)
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
         return self.km > self._value(other)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.km == self._value(other)
 
-    def __le__(self, other):
+    def __le__(self, other) -> bool:
         return self.km <= self._value(other)
 
-    def __ge__(self, other):
+    def __ge__(self, other) -> bool:
         return self.km >= self._value(other)
