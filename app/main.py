e2128eabdb5e853c4b76663f10cc6eede97b2d8f
Yatsenko Dmytro <sonituensem@gmail.com>
2025-11-17 12:01:27 +0200

    Fixed issues with type annotation for argument other

--- app/main.py
+++ app/main.py
@@ -8,12 +8,12 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other) -> "Distance":
+    def __add__(self, other: "Distance | float") -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other) -> "Distance":
+    def __iadd__(self, other: "Distance | float") -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
@@ -26,20 +26,20 @@ class Distance:
     def __truediv__(self, other: float) -> "Distance":
         return Distance(round(self.km / other, 2))
 
-    def _value(self, other) -> float:
+    def _value(self, other: "Distance | float") -> float:
         return other.km if isinstance(other, Distance) else other
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: "Distance | float") -> bool:
         return self.km < self._value(other)
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: "Distance | float") -> bool:
         return self.km > self._value(other)
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: "Distance | float") -> bool:
         return self.km == self._value(other)
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: "Distance | float") -> bool:
         return self.km <= self._value(other)
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: "Distance | float") -> bool:
         return self.km >= self._value(other)
